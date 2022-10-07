bd3cf107ea08345607d492ff835696cf0b845d9e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-07 10:57:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,18 @@ from streamlit_option_menu import option_menu
 
 # 1. as sidebar menu
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected = option_menu('Main Menu', ['Home', 'Upload', 'Tasks', 'Settings'], 
+        icons=['house', 'cloud-upload', 'list-task', 'gear'], menu_icon='cast', default_index=1)
     selected
 
 # 2. horizontal menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+selected2 = option_menu(None, ['Home', 'Upload', 'Tasks', 'Settings'], 
+    icons=['house', 'cloud-upload', 'list-task', 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
 
 # 3. CSS style definitions
-#selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+#selected3 = option_menu(None, ['Home', 'Upload',  'Tasks', 'Settings'], 
 #    icons=['house', 'cloud-upload', "list-task", 'gear'], 
 #    menu_icon="cast", default_index=0, orientation="horizontal",
 #    styles={
