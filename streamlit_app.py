4d158a38795867fac9560fdf248aa1fa80258b3d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-07 10:43:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ with st.sidebar:
 #selected2
 
 # 3. CSS style definitions
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    }
-)
+#selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+#    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+#    menu_icon="cast", default_index=0, orientation="horizontal",
+#    styles={
+#        "container": {"padding": "0!important", "background-color": "#fafafa"},
+#        "icon": {"color": "orange", "font-size": "25px"}, 
+#        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+#        "nav-link-selected": {"background-color": "green"},
+#    }
+#)
